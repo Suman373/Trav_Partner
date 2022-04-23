eafd97f552a963e3510c52f790d3122a9bb88841
Suman Roy <95040233+Suman373@users.noreply.github.com>
2022-04-23 21:58:24 +0530

    Add files via upload
    Hackathon project signup and login pages added

---
+++ Travel Partner/login.js
@@ -0,0 +1,20 @@
+ //password toggle
+ const togglePass = document.querySelector('#showPass');
+ const password = document.querySelector("#password");
+ var hide = document.getElementById("showPass");
+ 
+ togglePass.addEventListener('click', function(e) {
+     //toggling type attribute of the input
+     const type = password.getAttribute('type') === 'password' ? 'text' : 'password'; //ternary
+     password.setAttribute('type',type);
+     //toggling the content
+     if(hide.innerText === "Show"){
+         hide.innerText = "Hide";
+     }
+     else if(hide.innerText === "Hide"){
+         hide.innerText  = "Show";
+     }
+     else{
+         hide.innerText = "Show";
+     }
+ });
